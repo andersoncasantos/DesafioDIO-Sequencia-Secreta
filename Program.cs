222b8ef1e1d6f89b439daba6df2658a98ad35b09
Anderson <andersonmtv07@gmail.com>
2023-03-27 03:21:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+
+{
+    public static void Main()
+
+    {
+        int tamanhoSequencia = Convert.ToInt32(Console.ReadLine());
+        int quantidadeNumerosMarcados = 1;
+        int ultimoNumeroLido = Convert.ToInt32(Console.ReadLine());
+        int vi = 0;
+
+        for (int i = 1; i < tamanhoSequencia; i++)
+        {
+            vi = Convert.ToInt32(Console.ReadLine());
+            if (vi != ultimoNumeroLido)
+            {
+                quantidadeNumerosMarcados++;
+            }
+            ultimoNumeroLido = vi;
+        }
+        Console.WriteLine(quantidadeNumerosMarcados);
+    }
+}
